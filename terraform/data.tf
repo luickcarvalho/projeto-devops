8ba0019f9d63f36fe7a5af2d4075ceb362b0d03d
luickcarvalho <luick.carvalho@gmail.com>
2024-02-17 11:44:41 -0300

    Atualizado TF Data e Locals
    Criado data para retornar os ids das sub-redes e VPC para não deixar o ID Hardcode.

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,1 +1,36 @@
 data "aws_caller_identity" "current" {}
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["my_vpc"]
+  }
+}
+
+data "aws_subnet" "subnet-a" {
+  filter {
+    name   = "tag:Name"
+    values = ["subnet-prd-a"]
+  }
+}
+
+data "aws_subnet" "subnet-b" {
+  filter {
+    name   = "tag:Name"
+    values = ["subnet-prd-b"]
+  }
+}
+
+data "aws_subnet" "subnet-c" {
+  filter {
+    name   = "tag:Name"
+    values = ["subnet-prd-c"]
+  }
+}
+
+data "aws_subnet" "subnet-d" {
+  filter {
+    name   = "tag:Name"
+    values = ["subnet-prd-d"]
+  }
+}
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,7 @@
 locals {
   ### DYNAMIC VARS ###
-  vpc_id = "vpc-886a07f5"
+  vpc_id = data.aws_vpc.vpc.id
   
-  subnets_prd = ["subnet-53257a72", "subnet-c44aab88", "subnet-94a8facb", "subnet-eeabfa88"]
+  subnets_prd = [data.aws_subnet.subnet-a.id, data.aws_subnet.subnet-b.id, data.aws_subnet.subnet-c.id, data.aws_subnet.subnet-d.id]
 
 }
